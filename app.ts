48fe63f313d2f9366532b9ee99717e20fbb4d1b7
onifs10 <onifadebolu64@gmail.com>
2022-09-29 03:05:36 +0100

    update: added home page markup

--- app.ts
+++ app.ts
@@ -4,6 +4,9 @@ import path from "path";
 import dotenv from "dotenv";
 import * as prismicH from "@prismicio/helpers";
 import erroHandler from "errorhandler";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import methodOverride from "method-override";
 
 dotenv.config();
 
@@ -18,46 +21,65 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(erroHandler());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+// app.use(logger("dev"));
 app.use((req, res, next) => {
   res.locals.ctx = {
     prismicH,
+    NumbersH: {
+      indexMap: (num: number): string => {
+        return num == 0 ? "One" : "Two";
+      },
+    },
   };
   next();
 });
 
 app.get("/", async (req, res) => {
   // get home data from prismic
+  const preloader = await client.getSingle("preloader");
+  const home = await client.getSingle("home");
+  const meta = await client.getSingle("meta");
+  const collections = await client.getAllByType("collection");
   res.render("pages/home", {
-    meta: {
-      data: {
-        title: "Creative programming",
-        description: "Learning creative coding",
-      },
-    },
+    meta,
+    collections,
+    home,
+    preloader,
   });
 });
 
 app.get("/about", async (req, res) => {
-  const data = await client.getSingle("about");
+  const about = await client.getSingle("about");
+  const preloader = await client.getSingle("preloader");
   const meta = await client.getSingle("meta");
   res.render("pages/about", {
     meta,
-    about: data,
+    about,
+    preloader,
   });
 });
 
-app.get("/collections", (req, res) => {
+app.get("/collections", async (req, res) => {
+  const preloader = await client.getSingle("preloader");
+  const meta = await client.getSingle("meta");
+  const home = await client.getSingle("home");
+  const collections = await client.getAllByType("collection", {
+    fetchLinks: "detail.image",
+  });
   res.render("pages/collections", {
-    meta: {
-      data: {
-        title: "Creative programming",
-        description: "Learning creative coding",
-      },
-    },
+    meta,
+    collections,
+    home,
+    preloader,
   });
 });
 
 app.get("/details/:id", async (req, res) => {
+  const preloader = await client.getSingle("preloader");
   const product = await client.getByUID("detail", req.params.id, {
     fetchLinks: "collection.title",
   });
@@ -66,6 +88,7 @@ app.get("/details/:id", async (req, res) => {
   res.render("pages/detail", {
     meta,
     product,
+    preloader,
   });
 });
 app.listen(PORT, () => {
--- config/prismic.config.ts
+++ config/prismic.config.ts
@@ -1,8 +1,19 @@
 import fetch from "node-fetch";
 import * as prismic from "@prismicio/client";
 
+const routes = [
+  {
+    type: "detail",
+    path: "/details/:uid",
+  },
+  {
+    type: "about",
+    path: "/about",
+  },
+];
 export const createClient = (repoName: string, accessToken: string) =>
   prismic.createClient(repoName, {
     fetch,
     accessToken,
+    routes,
   });
